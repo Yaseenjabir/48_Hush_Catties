6be3c204a0cf822f7f0ce67324b0d5d6f74f7ab6
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-03-11 14:40:22 +0500

    init

--- Backend/Routes/reviewRoute.js
+++
@@ -1,7 +0,0 @@
-const createReview = require("../Controllers/ReviewController/createReview");
-
-const Router = require("express").Router();
-
-Router.post("/createReview", createReview);
-
-module.exports = Router;
